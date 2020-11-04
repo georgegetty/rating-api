329bc60a8ec088f3e2a6b8788f838ec093ab1b9c
georgegetty <73248527+georgegetty@users.noreply.github.com>
2020-11-04 14:24:36 -0500

    Update index.js
    Added new healthcheck endpoint called newone

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,17 @@ var jsonResponse = require('../models/jsonResponse');
 
 /* Default GET JSON for site */
 router.get('/', function(req, res, next) {
-  var response = new jsonResponse("Default ratings api endpoint", 503, []);
+  var response = new jsonResponse("Default ratings api endpoint", 200, []);
+  res.json(response).status(response.status);
+});
+
+router.get('/newone', function(req, res, next) {
+  var response = new jsonResponse("Default ratings api endpoint", 200, []);
+  res.json(response).status(response.status);
+});
+
+router.get('/healthcheck', function(req, res, next) {
+  var response = new jsonResponse("Ok", 200, []);
   res.json(response).status(response.status);
 });
 
