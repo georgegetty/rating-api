da8e04a922789cfe6449249138f5c314cc033ab0
georgegetty <73248527+georgegetty@users.noreply.github.com>
2020-11-04 14:13:51 -0500

    Update index.js
    Making default route fail for readiness check.

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var jsonResponse = require('../models/jsonResponse');
 
 /* Default GET JSON for site */
 router.get('/', function(req, res, next) {
-  var response = new jsonResponse("Default ratings api endpoint", 200, []);
+  var response = new jsonResponse("Default ratings api endpoint", 503, []);
   res.json(response).status(response.status);
 });
 
